42519823963f4bdee426bb29e03512c135f176f8
Himanshu Joshi <joshihimanshu747@gmail.com>
2025-01-21 17:04:38 +0530

    Message sending feature completed along with text and image

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -4,77 +4,79 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use App\Models\User;
+use App\Traits\FileUploadTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MessengerController extends Controller
 {
-    //
-    public function index(){
+    use FileUploadTrait;
+    public function index()
+    {
         return view('messenger.index');
     }
     /**
      * Search User Profiles
      */
-    public function userSearch(Request $request){
-       // dd($request->all());
-      // if($request->has('query')){
-        $input=$request['query'];
-        // dd($query);
-         $getRecords=null;
-         $records=User::where('id','!=',Auth::user()->id)
-             ->where('name','LIKE',"%{$input}%")
-             ->orWhere('user_name','LIKE',"%{$input}%")
-             ->paginate(10);
-         if($records->total() <1){
-            $getRecords.="<p class='text-center'>Nothing to show.</p>";
-         }       
-        foreach($records as $record){
-            $getRecords.=view('messenger.components.search-item',compact('record'))->render();
+    public function userSearch(Request $request)
+    {
+        $input = $request['query'];
+        $getRecords = null;
+        $records = User::where('id', '!=', Auth::user()->id)
+            ->where('name', 'LIKE', "%{$input}%")
+            ->orWhere('user_name', 'LIKE', "%{$input}%")
+            ->paginate(10);
+        if ($records->total() < 1) {
+            $getRecords .= "<p class='text-center'>Nothing to show.</p>";
+        }
+        foreach ($records as $record) {
+            $getRecords .= view('messenger.components.search-item', compact('record'))->render();
         }
         return response()->json([
-            'records'=>$getRecords,
-            'last_page'=>$records->lastPage()
+            'records' => $getRecords,
+            'last_page' => $records->lastPage()
         ]);
-       //}
+        //}
     }
-       //Fetch user by Id
-       public function fetchIdInfo(Request $request){
-            // dd($request->all());
-            $getUserInfo=User::where('id',$request['id'])->first();
+    //Fetch user by Id
+    public function fetchIdInfo(Request $request)
+    {
 
-            return response()->json(
-                [
-                    'getuserinfo'=>$getUserInfo
-                ]
-            );
-       }
+        $getUserInfo = User::where('id', $request['id'])->first();
 
-       function sendMessage(Request $request){
-            // dd($request->all());
-            $request->validate([
-                'message'=>['required'],
-                'id'=>['required','integer'],
-                'temporaryMsgId'=>['required']
-            ]);
-
-            //store message in db
-            $message=new Message();
-            $message->from_id=Auth::user()->id;
-            $message->to_id=$request->id;
-            $message->body=$request->message;
-            $message->save();
-
-            return response()->json([
-                'message'=>$this->messageCard($message),
-                'tempID'=>$request->temporaryMsgId
-            ]);
-
-
-       }
-       function messageCard($message){
-          return view('messenger.components.message-card',compact('message'))->render();
-       }
-
+        return response()->json(
+            [
+                'getuserinfo' => $getUserInfo
+            ]
+        );
     }
 
+    function sendMessage(Request $request)
+    {
+
+        $request->validate([
+            // 'message'=>['required'],
+            'id' => ['required', 'integer'],
+            'temporaryMsgId' => ['required'],
+            'attachment' => ['nullable', 'max:1024', 'image']
+        ]);
+
+        //store message in db
+        $attachmentPath = $this->uploadFile($request, 'attachment');
+        $message = new Message();
+        $message->from_id = Auth::user()->id;
+        $message->to_id = $request->id;
+        $message->body = $request->message;
+        if ($attachmentPath) $message->attachment = json_encode($attachmentPath);
+        $message->save();
+
+        return response()->json([
+            'message' => $message->attachment ? $this->messageCard($message, true) : $this->messageCard($message),
+            'tempID' => $request->temporaryMsgId
+        ]);
+    }
+    function messageCard($message, $attachment = false)
+    {
+        return view('messenger.components.message-card', compact('message', 'attachment'))->render();
+    }
+}
--- resources/js/messenger.js
+++ resources/js/messenger.js
@@ -3,143 +3,146 @@
  * Global variables
  *--------------------------------------
  */
-var temporaryMsgId=0;
+var temporaryMsgId = 0;
 
+const messageForm = $(".message-form"),
+    messageInput = $(".message-input"),
+    messageBoxContainer = $(".wsus__chat_area_body"),
+    csrf_token = $("meta[name=csrf_token]").attr("content");
 
-const messageForm= $(".message-form")
-,messageInput= $(".message-input"),
-messageBoxContainer=$(".wsus__chat_area_body"),
-csrf_token = $("meta[name=csrf_token]").attr("content");
-
-
-const getMessengerId=()=>$("meta[name=id]").attr("content");
-const setMessengerId=(id)=>$("meta[name=id]").attr("content",id);
-
-
-
+const getMessengerId = () => $("meta[name=id]").attr("content");
+const setMessengerId = (id) => $("meta[name=id]").attr("content", id);
 
 /**
  * -------------------------------------------
  * Resuable Functions
  * ------------------------------------------
  */
-function enableChatBoxLoader(){
-    $(".wsus__message_paceholder").removeClass('d-none');
+function enableChatBoxLoader() {
+    $(".wsus__message_paceholder").removeClass("d-none");
 }
-function disableChatBoxLoader(){
-    $(".wsus__message_paceholder").addClass('d-none');
+function disableChatBoxLoader() {
+    $(".wsus__message_paceholder").addClass("d-none");
 }
 
-function imagePreview(input,selector){
-    if(input.files && input.files[0]){
-        let render=new FileReader();
+function imagePreview(input, selector) {
+    if (input.files && input.files[0]) {
+        let render = new FileReader();
 
-        render.onload=function(e){
-            $(selector).attr('src',e.target.result);
-        }
+        render.onload = function (e) {
+            $(selector).attr("src", e.target.result);
+        };
         render.readAsDataURL(input.files[0]);
     }
 }
-let searchPage=1;
-let noMoreDataSearch=false;
-let searchTempVal="";
-let setSearchLoading=false;
-function searchUsers(query){
+let searchPage = 1;
+let noMoreDataSearch = false;
+let searchTempVal = "";
+let setSearchLoading = false;
+function searchUsers(query) {
     /**
      * Issue was facing while searching whenever we searching anything it was working fine
      * Second time we try to search it was facing issue while searching so it fixed by below code start
      */
-    if(query != searchTempVal){
-        searchPage=1;
-        noMoreDataSearch=false;
+    if (query != searchTempVal) {
+        searchPage = 1;
+        noMoreDataSearch = false;
     }
     /**
      * Issue was facing while searching whenever we searching anything it was working fine
      * Second time we try to search it was facing issue while searching so it fixed by below code end
      */
 
-    searchTempVal=query;
+    searchTempVal = query;
 
-    if(!setSearchLoading && !noMoreDataSearch){
+    if (!setSearchLoading && !noMoreDataSearch) {
         $.ajax({
-            method:'GET',
-            url:'/messenger/search',
-            data:{query:query,page:searchPage},
-            beforeSend:function(){
-                setSearchLoading=true;
-                let loader=`<div class="text-center search-loader"><div class="spinner-border text-primary" role="status"><span class="visually-hidden">Loading...</span></div></div>`;
-                $('.user_search_list_result').append(loader);
+            method: "GET",
+            url: "/messenger/search",
+            data: { query: query, page: searchPage },
+            beforeSend: function () {
+                setSearchLoading = true;
+                let loader = `<div class="text-center search-loader"><div class="spinner-border text-primary" role="status"><span class="visually-hidden">Loading...</span></div></div>`;
+                $(".user_search_list_result").append(loader);
             },
-            success:function(data){
-                setSearchLoading=false;
-                $('.user_search_list_result').find('.search-loader').remove();
-                if(searchPage < 2){
-                    $('.user_search_list_result').html(data.records);
+            success: function (data) {
+                setSearchLoading = false;
+                $(".user_search_list_result").find(".search-loader").remove();
+                if (searchPage < 2) {
+                    $(".user_search_list_result").html(data.records);
+                } else {
+                    $(".user_search_list_result").append(data.records);
                 }
-                else{
-                    $('.user_search_list_result').append(data.records);
-                }
-           
-                noMoreDataSearch = searchPage >= data?.last_page
-                if(noMoreDataSearch) searchPage+=1;
+
+                noMoreDataSearch = searchPage >= data?.last_page;
+                if (noMoreDataSearch) searchPage += 1;
             },
-            error:function(xhr,status,error){
-                setSearchLoading=false;
-                $('.user_search_list_result').find('.search-loader').remove();
-            }
+            error: function (xhr, status, error) {
+                setSearchLoading = false;
+                $(".user_search_list_result").find(".search-loader").remove();
+            },
         });
     }
-    
 }
 
-function actionOnScroll(selector,callback,topScroll=false){
-    $(selector).on('scroll',function(){
-        let element=$(this).get(0);
-        const condition=topScroll ? element.scrollTop==0 : element.scrollTop + element.clientHeight >= element.scrollHeight;
+function actionOnScroll(selector, callback, topScroll = false) {
+    $(selector).on("scroll", function () {
+        let element = $(this).get(0);
+        const condition = topScroll
+            ? element.scrollTop == 0
+            : element.scrollTop + element.clientHeight >= element.scrollHeight;
 
-        if(condition){
+        if (condition) {
             callback();
         }
     });
 }
-function debounce(callback,delay){
+function debounce(callback, delay) {
     let timerId;
-    return function(...args){
+    return function (...args) {
         clearTimeout(timerId);
-        timerId=setTimeout(()=>{
-            callback.apply(this,args);
-        },delay);
-    }
+        timerId = setTimeout(() => {
+            callback.apply(this, args);
+        }, delay);
+    };
 }
 /**
  * -------------------------------------------
  * Fetch id data of user and update the view
  * ------------------------------------------
  */
-function IDinfo(id){
+function IDinfo(id) {
     $.ajax({
-        method:"GET",
-        url:'messenger/id-info',
-        data:{id:id},
-        beforeSend:function(){
+        method: "GET",
+        url: "messenger/id-info",
+        data: { id: id },
+        beforeSend: function () {
             NProgress.start();
             enableChatBoxLoader();
         },
-        success:function(data){
+        success: function (data) {
             console.log(data);
-            $('.messenger-header').find("img").attr("src",data.getuserinfo.avatar);
-            $('.messenger-header').find("h4").text(data.getuserinfo.name);
+            $(".messenger-header")
+                .find("img")
+                .attr("src", data.getuserinfo.avatar);
+            $(".messenger-header").find("h4").text(data.getuserinfo.name);
 
-            $('.messenger-info-view .user_photo').find("img").attr("src",data.getuserinfo.avatar);
-            $('.messenger-info-view').find(".user_name").text(data.getuserinfo.name);
-            $('.messenger-info-view').find(".user_unique_name").text(data.getuserinfo.user_name);
+            $(".messenger-info-view .user_photo")
+                .find("img")
+                .attr("src", data.getuserinfo.avatar);
+            $(".messenger-info-view")
+                .find(".user_name")
+                .text(data.getuserinfo.name);
+            $(".messenger-info-view")
+                .find(".user_unique_name")
+                .text(data.getuserinfo.user_name);
             NProgress.done();
             disableChatBoxLoader();
         },
-        error:function(xhr,status,error){
+        error: function (xhr, status, error) {
             disableChatBoxLoader();
-        }
-    })
+        },
+    });
 }
 
 /**
@@ -147,93 +150,146 @@ function IDinfo(id){
  * Send Message
  * ------------------------------------------
  */
-function sendMessage(){
-    temporaryMsgId+=1;
-    let tempID=`temp_${temporaryMsgId}`;//temp_1
-    const inputValue=messageInput.val();
+function sendMessage() {
+    temporaryMsgId += 1;
+    let tempID = `temp_${temporaryMsgId}`; //temp_1
+    let hasAttachment = !!$(".attachment-input").val();
+    const inputValue = messageInput.val();
 
     //alert(inputValue);
-    if(inputValue.length>0){
+    if (inputValue.length > 0 || hasAttachment) {
         const formData = new FormData($(".message-form")[0]);
-        formData.append('id',getMessengerId());
-        formData.append("temporaryMsgId",tempID);
-        formData.append("_token",csrf_token);
-        
-       
+        formData.append("id", getMessengerId());
+        formData.append("temporaryMsgId", tempID);
+        formData.append("_token", csrf_token);
+
         $.ajax({
-            method:"POST",
-            url:"/messenger/send-message",
-            data:formData,
-            dataType:"JSON",
-            processData:false,
-            contentType:false,
-            beforeSend:function(){
+            method: "POST",
+            url: "/messenger/send-message",
+            data: formData,
+            dataType: "JSON",
+            processData: false,
+            contentType: false,
+            beforeSend: function () {
+                //console.log(hasAttachment);
+                if (hasAttachment) {
+                    messageBoxContainer.append(
+                        sendTempMessageCard(inputValue, tempID, true)
+                    );
+                } else {
+                    messageBoxContainer.append(
+                        sendTempMessageCard(inputValue, tempID)
+                    );
+                }
                 //add temp message on dom
-                messageBoxContainer.append(sendTempMessageCard(inputValue,tempID));
+
                 //messageForm.trigger("reset");
-                $(".emojionearea-editor").text("");
+                messageFormReset();
+                //$(".emojionearea-editor").text("");
             },
-            success:function(data){
-
+            success: function (data) {
+                const tempMsgCardElement = messageBoxContainer.find(
+                    `.message-card[data-id=${data.tempID}]`
+                );
+                tempMsgCardElement.before(data.message);
+                tempMsgCardElement.remove();
+                console.log(tempMsgCardElement);
             },
-            error:function(xhr,status,error){
-
-            }
+            error: function (xhr, status, error) {},
         });
     }
 }
 
-function sendTempMessageCard(message,tempId){
-    return `
-    <div class="wsus__single_chat_area" data-id="${tempId}">
+function sendTempMessageCard(message, tempId, attachment = false) {
+    if (attachment) {
+        return `<div class="wsus__single_chat_area message-card" data-id="${tempId}">
+                <div class="wsus__single_chat chat_right">
+                    <div class="pre_loader">
+                        <div class="spinner-border text-light" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                    ${
+                        message.length > 0
+                            ? `<p class="messages">${message}</p>`
+                            : ""
+                    }  
+                      <span class="clock"><i class="fas fa-clock"></i> now</span>
+                    <a class="action" href="#"><i class="fas fa-trash"></i></a>
+                </div>
+         </div>`;
+    }
+    return `<div class="wsus__single_chat_area message-card" data-id="${tempId}">
         <div class="wsus__single_chat chat_right">
             <p class="messages">${message}</p>
-            <span class="clock"><i class="fas fa-clock"></i> 5h ago</span>
+            <span class="clock"><i class="fas fa-clock"></i> now</span>
           
             <a class="action" href="#"><i class="fas fa-trash"></i></a>
         </div>
     </div>
-    `
+    `;
 }
+
+function messageFormReset() {
+    $(".attachment-block").addClass("d-none");
+    $(".emojionearea-editor").text("");
+    messageForm.trigger("reset");
+}
+/**
+ * Cancel selected attachment
+ */
+// function cancelAttachment(){
+
+// }
 /**
  * -------------------------------------------
  * On DOM Load
  * ------------------------------------------
  */
-$(document).ready(function(){
-    $('#select_file').change(function(){
-        imagePreview(this,'.profile-image-preview');
+$(document).ready(function () {
+    $("#select_file").change(function () {
+        imagePreview(this, ".profile-image-preview");
     });
-     //Search action on keyup
-     const debounceSearch= debounce(function(){
-        const value=$('.user_search').val();
+    //Search action on keyup
+    const debounceSearch = debounce(function () {
+        const value = $(".user_search").val();
         searchUsers(value);
-     },500);   
-    $('.user_search').on('keyup',function(){
-        let query=$(this).val();
-       if(query.length>0){
+    }, 500);
+    $(".user_search").on("keyup", function () {
+        let query = $(this).val();
+        if (query.length > 0) {
             debounceSearch();
-       }
+        }
     });
 
     //search pagination
-    actionOnScroll(".user_search_list_result",function(){
-        let value=$('.user_search').val();
-         searchUsers(value);
+    actionOnScroll(".user_search_list_result", function () {
+        let value = $(".user_search").val();
+        searchUsers(value);
     });
 
     //click action for messenger list item
-    $("body").on("click",".messenger-list-item",function(){
+    $("body").on("click", ".messenger-list-item", function () {
         //alert('selected');
-        const dataId=$(this).attr("data-id");
+        const dataId = $(this).attr("data-id");
         setMessengerId(dataId);
-        IDinfo(dataId); 
+        IDinfo(dataId);
     });
 
     //Send Message
 
-    $('.message-form').on("submit",function(e){
+    $(".message-form").on("submit", function (e) {
         e.preventDefault();
         sendMessage();
+    });
+
+    //send attachment
+
+    $(".attachment-input").change(function () {
+        imagePreview(this, ".attachment-preview");
+        $(".attachment-block").removeClass("d-none");
+    });
+    $(".cancel-attachment").on("click", function () {
+        messageFormReset();
     });
 });
